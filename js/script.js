0daaa3efdafe733a195010c3b8791c1ea6e8e2cb
Soheil <iamsoheil.r@gmail.com>
2025-03-04 22:16:21 +0330

    Make mobile navigation work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-const yearEL = document.querySelector(".year");
+// Set current year
+const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-yearEL.textContent = currentYear;
+yearEl.textContent = currentYear;
+
+// Mobile navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
